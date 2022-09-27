70c0897f5613f906c4a848a10f7c56e72d045fb6
radiadus <55176713+radiadus@users.noreply.github.com>
2022-09-27 15:43:05 +0700

    commit2

--- Test Suites/New Folder/suite myveego.ts
+++ Test Suites/New Folder/suite myveego.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>43569e58-ce28-47e4-8f1c-bb3129bbf0eb</testSuiteGuid>
+   <testCaseLink>
+      <guid>9bc87fa1-c30c-41f8-a305-6ce180e1ef57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login myveego</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
